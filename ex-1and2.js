133a82770ccfce91a88c2d276bfa8f40b6afa57f
bodin <bodinsonsawan@gmail.com>
2023-11-23 13:07:01 +0700

    fix(ex1-3)bodin's assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
-//Exercise #1: Variable Creation
-//Start coding here
+//ex1
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+//ex2
+const restaurantName = "Chef's Table by Chef James";
+const totalBillAmount = 15000;
+const openRestaurant = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBillAmount = 5000; // technically it is unable to re-assign due to declared by keyword 'const'
+openRestaurant = 2500; // technically it is unable to re-assign due to declared by keyword 'const'
